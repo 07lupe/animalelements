256ce1f008b274994e34c2fa4ec6dad6ac5530a0
mervinCSUB <89605110+mervinCSUB@users.noreply.github.com>
2021-10-04 11:29:01 -0700

    Add files via upload

---
+++ mervin.cpp
@@ -0,0 +1,25 @@
+//3350
+//Author:   Mario Ervin
+//Date:     9/30/2021
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+#include "fonts.h"
+//
+//
+void show_mervin_credits(int x, int y)
+{
+    Rect r;
+    r.bot = y;
+    r.left = x;
+    r.center = 1;
+    ggprint8b(&r, 16, 0x00fff000, "Mario Ervin");
+}
